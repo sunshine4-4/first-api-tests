9c2fa9d6ce66f55cd84a1169d5f737c12e8e6f6b
sunshine4-4 <04sveta12@gmail.com>
2024-11-25 23:50:19 +0400

    Сделала дополнительное задание 5_1

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,10 +1,26 @@
 from tests_class import TestCase
 
+
 class TestID:
 
     def test_first(self):
         test_case = TestCase()
         id_new_animal = test_case.create_animal('Orlock')
         id_find_animal = test_case.find_animal(id_new_animal)['id']
+
         assert id_new_animal == id_find_animal, (
             "[FAILED]: Ids doesnt match {} and {}".format(id_new_animal, id_find_animal))
+        
+    def test_second(self):
+        test_case = TestCase()
+        id_new_animal = test_case.create_animal('Rabbit')
+        id_find_animal = test_case.find_animal(id_new_animal)['id']
+
+        assert id_new_animal == id_find_animal, (
+            "[FAILED]: Ids doesnt match {} and {}".format(id_new_animal, id_find_animal))
+
+        test_case.remove_animal(id_new_animal)
+        message_find_animal = test_case.find_animal(id_new_animal)['message']
+
+        assert message_find_animal == "Pet not found", (
+            "[FAILED]: Animal with the id {} not removed".format(id_new_animal))
